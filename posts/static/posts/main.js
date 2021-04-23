2e12afc12c56480cec38d27898cb1ef2a3ff8295
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-04-23 13:47:55 -0400

    created card body using main.js, and added a loading spinner

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('hello');
 
 const helloWorldBox = document.getElementById('hello-world');
 const postsBox = document.getElementById('posts-box');
+const spinnerBox = document.getElementById('spinner-box');
 
 // GET
 $.ajax({
@@ -23,13 +24,32 @@ $.ajax({
     success: function(response){
         console.log(response)
         const data = response.data  // Data is being passed by the backend view
-        console.log(data)
-        // for each method on data
-        data.forEach(el => {
-            postsBox.innerHTML += `
-                ${el.title} - <b>${el.body}</b><br/>
-            `
-        });
+        setTimeout(() => {
+            spinnerBox.classList.add("not-visible"); // When data is loaded make spinnerBox not-visible
+            console.log(data);
+            // for each method on data
+            data.forEach((el) => {
+                postsBox.innerHTML += `
+                    <div class="card mb-2" >
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
+                        </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-xs-2 col-md-1">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-xs-2 col-md-1">
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+            `;
+                    });
+        }, 100);
+
     },
     error: function(error){
         console.log(error)
